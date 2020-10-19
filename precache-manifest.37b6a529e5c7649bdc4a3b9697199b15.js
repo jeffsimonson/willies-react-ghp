8099ca1f4afa5a4ddeac2e4271b78491a0e23df9
Jeff Simonson <jeffsimonson@live.com>
2020-10-19 02:13:34 -0500

    Updates

---
+++ precache-manifest.37b6a529e5c7649bdc4a3b9697199b15.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047a1fc6de6f87c23fcaa19c4aab1f4a",
+    "url": "/willies-react-ghp/index.html"
+  },
+  {
+    "revision": "bfb9f3ab92537e80f418",
+    "url": "/willies-react-ghp/static/css/main.043a7933.chunk.css"
+  },
+  {
+    "revision": "704b0ca30d318dac1380",
+    "url": "/willies-react-ghp/static/js/2.ce83c876.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/willies-react-ghp/static/js/2.ce83c876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb9f3ab92537e80f418",
+    "url": "/willies-react-ghp/static/js/main.ae22bb3b.chunk.js"
+  },
+  {
+    "revision": "2f366de5bf8f83d396ba",
+    "url": "/willies-react-ghp/static/js/runtime-main.a13e3ecf.js"
+  },
+  {
+    "revision": "909ea5323764b3b3a2bfaec08e035a4e",
+    "url": "/willies-react-ghp/static/media/Courgette-Regular.909ea532.ttf"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/willies-react-ghp/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/willies-react-ghp/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "2fd9c16b805724d590c0cff96da070a4",
+    "url": "/willies-react-ghp/static/media/NotoSans-Regular.2fd9c16b.ttf"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/willies-react-ghp/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "3352653dedd571bbc490c8be132b38cd",
+    "url": "/willies-react-ghp/static/media/Poppins-Light.3352653d.ttf"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/willies-react-ghp/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/willies-react-ghp/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "385a87f076383fe6cdd92ef2c706c35b",
+    "url": "/willies-react-ghp/static/media/Willlie-Iggers-photo.385a87f0.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/willies-react-ghp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/willies-react-ghp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/willies-react-ghp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/willies-react-ghp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/willies-react-ghp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d3864fe4ef1ba216c187c43068e937bc",
+    "url": "/willies-react-ghp/static/media/pattern2.d3864fe4.png"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/willies-react-ghp/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/willies-react-ghp/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/willies-react-ghp/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/willies-react-ghp/static/media/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "4c6b9628adf6b8b9820cc82472984d5c",
+    "url": "/willies-react-ghp/static/media/willies-bg-about.4c6b9628.jpg"
+  },
+  {
+    "revision": "235e642edff0c3bab2aee963244c4669",
+    "url": "/willies-react-ghp/static/media/willies-bg-contact.235e642e.jpg"
+  },
+  {
+    "revision": "a7cb3bda26748a117eea148f2d7ef57c",
+    "url": "/willies-react-ghp/static/media/willies-bg-events.a7cb3bda.jpg"
+  },
+  {
+    "revision": "87500e53c06630aad4afb9b9f7562c02",
+    "url": "/willies-react-ghp/static/media/willies-bg-food.87500e53.jpg"
+  },
+  {
+    "revision": "3721b1bd22fdd54d55e3f59d35031a1e",
+    "url": "/willies-react-ghp/static/media/willies-bg-gallery.3721b1bd.jpg"
+  },
+  {
+    "revision": "85f919b65034e9ee87a304905c0d16a8",
+    "url": "/willies-react-ghp/static/media/willies-bg-wines.85f919b6.jpg"
+  },
+  {
+    "revision": "6e81f689c6904e48e70d07859d7dd7bf",
+    "url": "/willies-react-ghp/static/media/willies-chef.6e81f689.jpg"
+  },
+  {
+    "revision": "5862cb67c0a3a8a47ccce7f033851fc0",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-01.5862cb67.jpg"
+  },
+  {
+    "revision": "f8a784fdbd1a5f080659f74d4e475c50",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-02.f8a784fd.jpg"
+  },
+  {
+    "revision": "afc00882b4705b3f7ee2d6d1d63205f7",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-03.afc00882.jpg"
+  },
+  {
+    "revision": "83ce018f1e0208872d40487a2847adee",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-04.83ce018f.jpg"
+  },
+  {
+    "revision": "10b92892babb476dc84fc60608ad0eef",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-05.10b92892.jpg"
+  },
+  {
+    "revision": "f658a8a80e233c7c288bbf067d86f8b6",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-06.f658a8a8.jpg"
+  },
+  {
+    "revision": "3742e049f9ec8ce184607a1840295bba",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-07.3742e049.jpg"
+  },
+  {
+    "revision": "353f257bdc3adcf90e48d50738fa486d",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-08.353f257b.jpg"
+  },
+  {
+    "revision": "e08539fa0271397c194024655ec08dc9",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-09.e08539fa.jpg"
+  },
+  {
+    "revision": "e831f213b85443fb6a26aa15280f37ed",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-10.e831f213.jpg"
+  },
+  {
+    "revision": "d95d87e25a931e40cf40ad6709825c48",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-11.d95d87e2.jpg"
+  },
+  {
+    "revision": "01333e960435c652d9543cf046308b4b",
+    "url": "/willies-react-ghp/static/media/willies-gallery-big-12.01333e96.jpg"
+  },
+  {
+    "revision": "287c04e1e0327ea19ad41f5f6938eb28",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-01.287c04e1.jpg"
+  },
+  {
+    "revision": "3f93f83022f449aadaa08b7ec2d601a8",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-02.3f93f830.jpg"
+  },
+  {
+    "revision": "5ef34af0831121d1a959c0737ac68995",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-03.5ef34af0.jpg"
+  },
+  {
+    "revision": "b772aadd21034d8743b10f7dd2a4637d",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-04.b772aadd.jpg"
+  },
+  {
+    "revision": "944c8add10f0c99f887612593e36aa09",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-05.944c8add.jpg"
+  },
+  {
+    "revision": "7a8f74dfacc9c6613d1f34b1598b7c79",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-06.7a8f74df.jpg"
+  },
+  {
+    "revision": "791fe134ebd4dc435411a637abdce2bd",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-07.791fe134.jpg"
+  },
+  {
+    "revision": "aa927317bf007040e45beda9464d38bc",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-08.aa927317.jpg"
+  },
+  {
+    "revision": "bcb4588bcf44ebed7af6f7d48d9182fd",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-09.bcb4588b.jpg"
+  },
+  {
+    "revision": "2926afbeeaf74be27afac5988ad38bf4",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-10.2926afbe.jpg"
+  },
+  {
+    "revision": "bcd6127e15a3eacad697fb1e7a2e6d69",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-11.bcd6127e.jpg"
+  },
+  {
+    "revision": "07c7834d58fa5a253deac1233a799c89",
+    "url": "/willies-react-ghp/static/media/willies-gallery-thumb-12.07c7834d.jpg"
+  },
+  {
+    "revision": "e08539fa0271397c194024655ec08dc9",
+    "url": "/willies-react-ghp/static/media/willies-jazz-band.e08539fa.jpg"
+  },
+  {
+    "revision": "5862cb67c0a3a8a47ccce7f033851fc0",
+    "url": "/willies-react-ghp/static/media/willies-master-slide-01.5862cb67.jpg"
+  },
+  {
+    "revision": "f8a784fdbd1a5f080659f74d4e475c50",
+    "url": "/willies-react-ghp/static/media/willies-master-slide-02.f8a784fd.jpg"
+  },
+  {
+    "revision": "afc00882b4705b3f7ee2d6d1d63205f7",
+    "url": "/willies-react-ghp/static/media/willies-master-slide-03.afc00882.jpg"
+  },
+  {
+    "revision": "353f257bdc3adcf90e48d50738fa486d",
+    "url": "/willies-react-ghp/static/media/willies-our-story.353f257b.jpg"
+  },
+  {
+    "revision": "83ce018f1e0208872d40487a2847adee",
+    "url": "/willies-react-ghp/static/media/willies-small-plates.83ce018f.jpg"
+  },
+  {
+    "revision": "10b92892babb476dc84fc60608ad0eef",
+    "url": "/willies-react-ghp/static/media/willies-wine-bottles.10b92892.jpg"
+  },
+  {
+    "revision": "69322ba32d24924a1596768779a22eae",
+    "url": "/willies-react-ghp/static/media/willies-wine-cellar.69322ba3.jpg"
+  },
+  {
+    "revision": "fddd39a0d733ed70a11682dbcf47cf75",
+    "url": "/willies-react-ghp/static/media/willies-wine-school.fddd39a0.jpg"
+  }
+]);
